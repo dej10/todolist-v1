c180a93344ed99e5e1ce3e96be054dab41a999f6
Ayodeji <46384451+dej10@users.noreply.github.com>
2020-10-10 22:44:16 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+
+let todoLists = [ " Eat Food", "Drink Water" ];
+
+
+app.get("", function (req, res) {
+const today = new Date();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+let day = today.toLocaleDateString("en-US", options);
+
+res.render("lists", {
+	dayName : day,
+	listItems : todoLists
+});
+
+});
+
+
+app.post("", function (req, res) {
+
+	let todoList = req.body.listItem;
+
+	todoLists.push(todoList);
+
+	res.redirect("");
+});
+
+
+
+app.listen(3000, function(){
+	console.log("Server is running on port 3000 ......")
+});
