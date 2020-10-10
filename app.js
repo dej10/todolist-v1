ce2a2e661a62c6417c547686c73f058486b76ac3
Ayodeji <46384451+dej10@users.noreply.github.com>
2020-10-10 22:56:15 +0100

    updating /views

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-let todoLists = [ " Eat Food", "Drink Water" ];
+let todoLists = [];
 
 
 app.get("", function (req, res) {
